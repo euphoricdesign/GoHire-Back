8c343f24fa15e5b8acbeaf5a2e13ac6e377ee6c6
Merlina <noahchukita@gmail.com>
2024-08-07 16:45:23 -0300

    feat(config): agregar propiedad allowedHeaders a la configuración de CORS

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,10 @@ const port = process.env.PORT || 3001;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://go-hire-flax.vercel.app/'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: ['http://localhost:3000', 'https://go-hire-flax.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });  
 
   const swaggerConfig = new DocumentBuilder()
